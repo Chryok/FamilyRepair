1c3b7392042e6a14ca72f2fbe41eea94060c7110
Chryok (Chris F.) <TheChryok@gmail.com>
2022-11-17 12:55:40 -0700

    Scroll scale fix and compensation for padding

--- FamilyRepairApp/Assets/Scripts/VerticalScaleToScreenHeight.cs
+++ FamilyRepairApp/Assets/Scripts/VerticalScaleToScreenHeight.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class VerticalScaleToScreenHeight : MonoBehaviour
 {
     [SerializeField]
-    private float ScreenPercentToScaleToV, ScreenPercentToScaleToH;
+    private bool UseAnotherRectAsRef = false;
+
+    [SerializeField]
+    private RectTransform refRectTransform;
+
+    [SerializeField]
+    private int PaddingCompensationV = 0;
 
     [SerializeField]
     private bool ConstantUpdate = false;
@@ -13,17 +19,30 @@ public class VerticalScaleToScreenHeight : MonoBehaviour
     [SerializeField]
     private bool ScaleHorizontal = false;
 
+    [SerializeField]
+    private float ScreenPercentToScaleToV, ScreenPercentToScaleToH;
+
     private RectTransform rectTransform;
+    private Vector2 refSize;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (UseAnotherRectAsRef)
+        {
+            refSize = new Vector2(refRectTransform.rect.width, refRectTransform.rect.height - PaddingCompensationV);
+        }
+        else
+        {
+            refSize = new Vector2(Screen.width, Screen.height - PaddingCompensationV);
+        }
+
         rectTransform = this.GetComponent(typeof(RectTransform)) as RectTransform;
-        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, Screen.height * ScreenPercentToScaleToV);
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, (refSize.y * ScreenPercentToScaleToV) - PaddingCompensationV);
 
         if (ScaleHorizontal)
         {
-            rectTransform.sizeDelta = new Vector2(Screen.width * ScreenPercentToScaleToH, rectTransform.sizeDelta.y);
+            rectTransform.sizeDelta = new Vector2(refSize.x * ScreenPercentToScaleToH, rectTransform.sizeDelta.y);
         }
 
     }
@@ -33,11 +52,20 @@ public class VerticalScaleToScreenHeight : MonoBehaviour
     {
         if (ConstantUpdate)
         {
-            rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, Screen.height * ScreenPercentToScaleToV);
+            if (UseAnotherRectAsRef)
+            {
+                refSize = new Vector2(refRectTransform.rect.width, refRectTransform.rect.height - PaddingCompensationV);
+            }
+            else
+            {
+                refSize = new Vector2(Screen.width, Screen.height - PaddingCompensationV);
+            }
+
+            rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, (refSize.y * ScreenPercentToScaleToV) - PaddingCompensationV);
 
             if (ScaleHorizontal)
             {
-                rectTransform.sizeDelta = new Vector2(Screen.width * ScreenPercentToScaleToH, rectTransform.sizeDelta.y);
+                rectTransform.sizeDelta = new Vector2(refSize.x * ScreenPercentToScaleToH, rectTransform.sizeDelta.y);
             }
         }
     }
